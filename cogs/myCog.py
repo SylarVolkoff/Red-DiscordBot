a4b041bb3123eacb8286f3e1f01c02b592b2b3d8
Gray <xxbloodyhell478@gmail.com>
2016-05-03 16:47:08 -0400

    :3.... just incase imports and comments
    Pls work?

--- cogs/myCog.py
+++ cogs/myCog.py
@@ -1,21 +1,28 @@
 import discord
 from discord.ext import commands
 from .utils.dataIO import fileIO
+from .utils import checks
+import os
+import time
+import aiohttp
+import asyncio
+from copy import deepcopy
+import logging
 
 class Mycog:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.player_names = fileIO("data/namess/log.json", "load")
+        #self.player_names = fileIO("data/namess/log.json", "load")
 
     @commands.command()
-    async def mycom(self, fName: str, lName: str, lv: str):
+    async def mycom(self, fname: str, lname: str, lv: str):
         """This does stuff!"""
-        doge = fName + " " + lName + " " + lv
+        doge = fname + " " + lname + " " + lv
         #Your code will go here
         await self.bot.say("Your information will be recorded as: " + doge)
-        fileIO("data/namess/log.json", "save", doge)
+        #fileIO("data/namess/log.json", "save", doge)
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
