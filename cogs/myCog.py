d34b2212f335c34efcae66220de516f42dab06dd
Gray <xxbloodyhell478@gmail.com>
2016-05-03 16:41:52 -0400

    Cool so. Testing both string fun and logging.
    Testing the logging of information into a json file. Honestly if this
    works I'll scream and get fired but I do not really expect it to. The
    excitement is still there though >:3

--- cogs/myCog.py
+++ cogs/myCog.py
@@ -1,18 +1,21 @@
 import discord
 from discord.ext import commands
+from .utils.dataIO import fileIO
 
 class Mycog:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.player_names = fileIO("data/namess/log.json", "load")
 
     @commands.command()
-    async def mycom(self, cat: str):
+    async def mycom(self, fName: str, lName: str, lv: str):
         """This does stuff!"""
-
+        doge = fName + " " + lName + " " + lv
         #Your code will go here
-        await self.bot.say("I can do stuff! " + cat)
+        await self.bot.say("Your information will be recorded as: " + doge)
+        fileIO("data/namess/log.json", "save", doge)
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
